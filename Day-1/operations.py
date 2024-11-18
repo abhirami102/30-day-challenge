084e84734ac7acf748ad3d195e2ead7d2b46837c
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-11-18 20:37:43 +0530

    Add files via upload

---
+++ Day-1/operations.py
@@ -0,0 +1,10 @@
+num1=int(input("num1="))
+num2=int(input("num2="))
+sum=num1+num2
+print("The sum is",sum)
+differnce=num1-num2
+print("The difference is",differnce)
+product=num1*num2
+print("The prouct is",product)
+quotient=num1/num2
+print('The quotient is',quotient)
---
+++ Day-1/str.py
@@ -0,0 +1,5 @@
+str1="hello world"
+str2=str1[6:]
+print(str2)
+new_string=str2+" everyone"
+print("The modified text is:",new_string)
