7767ed461e5a702220c985fba646067b6c6e5108
Alwin8969 <alwinjoshy2028@cs.sjcetpalai.ac.in>
2024-11-18 20:13:00 +0530

    Add files via upload

---
+++ day-2/day2.py
@@ -0,0 +1,10 @@
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+sum=num1+num2
+subtraction=num1-num2
+multiplication =num1*num2
+division=num1/num2
+print("Sum=", sum)
+print("Subtraction=", subtraction)
+print("Multiplication=", multiplication)
+print("Division=", division)
