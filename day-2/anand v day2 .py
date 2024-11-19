85e727769720980edc053860207dbad193364f0f
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-11-19 16:12:59 +0530

    Add files via upload

---
+++ day-2/anand v day2 .py
@@ -0,0 +1,15 @@
+# Define variables
+num1 = 8
+num2 = 3
+
+# Perform calculations
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+
+# Display results with labels
+print("The sum is", sum_result)
+print("The difference is", difference_result)
+print("The product is", product_result)
+print("The quotient is", quotient_result)
