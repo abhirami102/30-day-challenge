1fc667cbb30e89e2fd8e68e41612762fb3b22199
Alwin8969 <alwinjoshy2028@cs.sjcetpalai.ac.in>
2024-12-05 22:00:59 +0530

    Add files via upload

---
+++ Day-8/day 8 alwin joshy.py
@@ -0,0 +1,7 @@
+list1 = [5, 3, 8, 1]
+list2 = [7, 2, 6, 4]
+merged_list = list1 + list2  
+merged_list.sort()  
+largest_number = merged_list[-1] 
+print("The merged and sorted list is:", merged_list)
+print("The largest number in the list is:", largest_number)
